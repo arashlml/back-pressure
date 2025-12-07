3ba6d3cffa5fa95b5e2d6408e4c42adb28545bf9
arash <aarashlml78@gmail.com>
2025-12-07 18:00:25 +0330

    fix: backpressure logic fixed

--- back-pressure.go
+++ back-pressure.go
@@ -6,47 +6,35 @@ import (
 )
 
 type BackPressure[T any] struct {
-	maxSizeChannel      int64
-	thresholdPercentage int64
-	channel             chan T
-	wait                time.Duration
-	quite               chan struct{}
+	bufferSize int64
+	channel    chan T
+	quit       chan struct{}
+	AddCounter int64
 }
 
-func NewBackPressure[T any](maxSizeChannel, thresholdPercentage int64, wait time.Duration) *BackPressure[T] {
-	log.Println("making the backpressure")
+func NewBackPressure[T any](bufferSize, thresholdPercentage int64, wait time.Duration) *BackPressure[T] {
 	b := &BackPressure[T]{
-		maxSizeChannel:      maxSizeChannel,
-		thresholdPercentage: thresholdPercentage,
-		channel:             make(chan T, maxSizeChannel),
-		wait:                wait,
-		quite:               make(chan struct{}),
+		bufferSize: bufferSize,
+		channel:    make(chan T, bufferSize),
+		quit:       make(chan struct{}),
 	}
-	go b.Wait()
-	go b.ShotDown()
 	return b
-}
-func (b *BackPressure[T]) ShotDown() {
-
-	<-b.quite
-
-	if len(b.channel) == 0 {
-		log.Println("BACK PRESSURE: CHANNEL IS EMPTY NOW I CLOSE IT")
-		close(b.channel)
-	}
-
 }
 
 func (b *BackPressure[T]) Add(item T) {
-	b.channel <- item
-}
-func (b *BackPressure[T]) Wait() {
+
 	for {
-		if int64(len(b.channel)) >= b.thresholdPercentage*b.maxSizeChannel/100 {
-			log.Println("channel has reached it's threshold")
-			time.Sleep(b.wait)
-		} else {
-			log.Println("channel is open")
+		if int64(len(b.channel)) == b.bufferSize {
+			log.Printf("BACK PRESSURE: Buffer is full... producer is screaming internally 😤")
+
+		}
+		select {
+		case b.channel <- item:
+			b.AddCounter++
+			log.Printf("BACK PRESSURE: Added %v items \n", b.AddCounter)
+		case <-b.quit:
+			close(b.channel)
+			return
 		}
 	}
 }
@@ -56,5 +44,6 @@ func (b *BackPressure[T]) Out() chan T {
 }
 
 func (b *BackPressure[T]) Close() {
-	close(b.quite)
+	close(b.quit)
+
 }
